9c5324e1b4369f8a222c0515b3a5fd7807ecd19f
Sornk <598839297@qq.com>
2024-04-18 20:07:06 +0800

    docs: update leaderboard 20240418

--- eval/eval_track3.py
+++ eval/eval_track3.py
@@ -1,5 +1,4 @@
 from typing import List, Optional, Dict
-
 from sklearn.metrics import f1_score
 
 from eval.evaluate import Evaluate
@@ -23,6 +22,13 @@ class EvaluateTrack3(Evaluate):
 
         return vector
 
+    @staticmethod
+    def calculate_f1_score(truth_list: List[List[int]], pred_list: List[List[int]]) -> float:
+        f1 = 0.0
+        for truth, pred in zip(truth_list, pred_list):
+            f1 += f1_score(truth, pred, average='binary', zero_division=1.0)
+        return f1 / len(truth_list)
+
     def get_vector(self, prediction: List[Dict[str, str]], key_name: str, sentence: str) -> List[int]:
         begin_idx = self.get_prediction(prediction, f'{key_name}BeginIdx')
         end_idx = self.get_prediction(prediction, f'{key_name}EndIdx')
@@ -43,8 +49,8 @@ class EvaluateTrack3(Evaluate):
             truth_tenor_list.append(self.get_vector(truth['prediction'], 'tenor', truth['sentence']))
             truth_vehicle_list.append(self.get_vector(truth['prediction'], 'vehicle', truth['sentence']))
 
-        conjunction_f1 = f1_score(truth_conjunction_list, pred_conjunction_list, average='macro', zero_division=1.0)
-        tenor_f1 = f1_score(truth_tenor_list, pred_tenor_list, average='macro', zero_division=1.0)
-        vehicle_f1 = f1_score(truth_vehicle_list, pred_vehicle_list, average='macro', zero_division=1.0)
+        conjunction_f1 = self.calculate_f1_score(truth_conjunction_list, pred_conjunction_list)
+        tenor_f1 = self.calculate_f1_score(truth_tenor_list, pred_tenor_list)
+        vehicle_f1 = self.calculate_f1_score(truth_vehicle_list, pred_vehicle_list)
 
         return (conjunction_f1 + tenor_f1 + vehicle_f1) / 3.0
